05690ce0ff5a61798f91f17c78a7c1bfbe567ec4
PVo <phucv2376@gmail.com>
2024-11-01 21:08:03 -0500

    Sprint 3 test update

--- Sprint3/test/SimpleGameOverTest.java
+++ Sprint3/test/SimpleGameOverTest.java
@@ -8,26 +8,26 @@ public class SimpleGameOverTest {
         Player blue = new Player("blue", 'S');
         Player red = new Player("red", 'S');
         SOSLogic gameConds = new SOSSimpleLogic(blue, red);
-        gameConds.startGame();
-        gameConds.makePlayerMove(1, 0); //B
-        gameConds.makePlayerMove(0, 0); //R
+        blue.setLetter('S');
         red.setLetter('O');
-        gameConds.makePlayerMove(1, 2); //B
-        gameConds.makePlayerMove(0, 1); //R
-        gameConds.makePlayerMove(1, 1); //B
-        red.setLetter('S');
-        gameConds.makePlayerMove(0, 2); //R
-        assertEquals(GameState.REDWIN,gameConds.getState());
+        gameConds.startGame();
 
+        gameConds.makePlayerMove(1, 0); //B
+        gameConds.makePlayerMove(1, 1); //R
+        gameConds.makePlayerMove(0, 0); //B
+        red.setLetter('S');
+        gameConds.makePlayerMove(1, 2); //R
+        assertEquals(GameState.REDWIN,gameConds.getState());
     }
     @Test
     void TestBlueWinVerticalSimple() {
         Player blue = new Player("blue", 'S');
         Player red = new Player("red", 'S');
         SOSLogic gameConds = new SOSSimpleLogic(blue, red);
-        gameConds.startGame();
-        gameConds.makePlayerMove(0, 0); //B
         red.setLetter('O');
+        gameConds.startGame();
+
+        gameConds.makePlayerMove(0, 0); //B
         gameConds.makePlayerMove(1, 0); //R
         gameConds.makePlayerMove(2, 0); //B
         assertEquals(GameState.BLUEWIN,gameConds.getState());
